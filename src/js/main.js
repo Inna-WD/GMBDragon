f0e64e600809062e702bf909cb21ad09388cd75e
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-02-27 11:33:05 -0500

    added new js file for each page eventslistener

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+// functions
+
+function menuVisible() /* show menu on click in mobile version */
+{
+    document.getElementById("search").classList.toggle("visible");
+    document.getElementById("account").classList.toggle("invisible");
+    document.getElementById("mainmenu").classList.toggle("visible");
+    let menuValue= document.getElementById("menu").innerHTML;
+    if (menuValue == 'menu')
+    {
+        document.getElementById("menu").innerHTML = 'clear';
+    }
+    else if (menuValue=='clear')
+    {
+        document.getElementById("menu").innerHTML = 'menu';
+    }
+    
+}
+function searchVisible() /* make search on tablet screen size */
+{
+   if (window.innerWidth >= 650 && window.innerWidth <= 1200)
+   {
+    // document.getElementById("search").children[0].classList.toggle("notVisible");
+    document.getElementById("search").children[1].classList.toggle("notVisible");
+    document.getElementById("search").children[1].classList.toggle("makeVisible");  
+   }
+}
+
+
+window.addEventListener("load", ()=> { /* things to be done when page is loaded */
+    let dropMenu = document.getElementById("menu");
+    dropMenu.addEventListener("click", menuVisible);
+    let searchMenu = document.getElementById("srchicon");
+    searchMenu.addEventListener("click", searchVisible);
+    
+});
