a2b4c2cd73cfc8ad1fde87a83260ef4360304936
Inna Sarra Manova <n01384347@nj-00118-021.humber.org>
2020-03-03 18:54:35 -0500

    initial programs page

--- src/components/programs.js
+++ src/components/programs.js
@@ -3,7 +3,24 @@ import { html } from "../../core/Spaf.js";
 const ProgramsPage = () => {
   console.log("Programs page");
   return html`
-    <h1>Programs page!!</h1>
+  <main role="main" class="home">
+  <section class="hero">
+    <div class="program-banner">
+      <h1 class="hero-text">Web Design and Development</h1> <br>
+    </div>
+    <div class="program-info-nav">
+    </section>
+<nav>
+<div class="program-button"> CURRICULIUM</div>
+<div class="program-button"> ACADEMIC STUFF</div>
+<div class="program-button"> ADMISSION REQUIREMENTS</div>
+<div class="program-button"> FEES</div>
+<div class="program-button"> HOW TO APPLY</div>
+</nav>
+
+
+
+
   `;
 };
 
