728621206f5d4799efd6a958644f6c582e85658e
Inna Sarra Manova <n01384347@nj-00118-021.humber.org>
2020-03-03 20:23:21 -0500

    info section

--- src/components/programs.js
+++ src/components/programs.js
@@ -8,15 +8,49 @@ const ProgramsPage = () => {
     <div class="program-banner">
       <h1 class="hero-text">Web Design and Development</h1> <br>
     </div>
-    <div class="program-info-nav">
-    </section>
-<nav>
-<div class="program-button"> CURRICULIUM</div>
-<div class="program-button"> ACADEMIC STUFF</div>
-<div class="program-button"> ADMISSION REQUIREMENTS</div>
-<div class="program-button"> FEES</div>
-<div class="program-button"> HOW TO APPLY</div>
+  </section>
+
+<nav class="program-info-nav">
+        <div></div>
+        <div class="program-button"> CURRICULIUM</div>
+        <div class="program-button"> ACADEMIC STUFF</div>
+        <div class="program-button"> ADMISSION REQUIREMENTS</div>
+        <div class="program-button"> FEES</div>
+        <div class="program-button"> HOW TO APPLY</div>
+        <div></div>
 </nav>
+
+<div>
+    If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+    Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
+    You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+    This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+</div>
+
+
+<section class="program-window">
+        <div class="program-block"> 
+            Program Benefits<br>
+            * Become a career-ready web designer in 1 year
+            * Gain a comprehensive education from highly-talented and industry-experienced faculty
+            * Use various industry-standard software in our state-of-the-art labs * Graduate from the program with a captivating portfolio website to present to potential employers
+            * Make connections with industry professionals during networking opportunities
+        </div>
+
+        <div class="program-block"> 
+            Program Benefits<br>
+            * Become a career-ready web designer in 1 year
+            * Gain a comprehensive education from highly-talented and industry-experienced faculty
+            * Use various industry-standard software in our state-of-the-art labs * Graduate from the program with a captivating portfolio website to present to potential employers
+            * Make connections with industry professionals during networking opportunities
+        </div>
+
+</section>
+
+
+
+
+
 
 
 
