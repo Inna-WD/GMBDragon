b6aac5b57d2d81b3b0d6ff62bb28c37d703c12ca
Bijaya Gautam <n01399849@nj-00118-020.humber.org>
2020-03-04 21:50:57 -0500

    Added program selection and sort by year, name

--- src/components/students.js
+++ src/components/students.js
@@ -3,34 +3,72 @@ import { html } from "../../core/Spaf.js";
 const StudentsPage = () => {
   console.log("Students page");
   return html`
-    <main class="class-of">
-    <h1 class="graduation">Class of 2020 </h1>
-        <section class="all-students">
-            <article class="student-basic">
-                <a href="/studentProfile" data-route="/studentProfile"><img src="/img/bijaya_pro_pic.png" alt="professional image" class="student-image"></a>
-                <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Bijaya Gautam</h1></a>
-                <h2 class="student-position">App System Engineer</h2>
-            </article>
+    <main>
+        <div class="student-program-choice">
+            <label class="program-choice">
+                <input type="radio" name="program" value="webDesign" checked>
+                <span>Web Design and Development</span>
+            </label>
 
-            <article class="student-basic">
-                <a href="/studentProfile" data-route="/studentProfile"><img src="/img/joseph_pro_pic.jpg" alt="professional image" class="student-image"></a>
-                <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Joseph Lao</h1></a>
-                <h2 class="student-position">Software Engineer</h2>
-            </article>
+            <label class="program-choice">
+                <input type="radio" name="program" value="grahicDesign">
+                <span>Graphic Design for Print and Web</span>
+            </label>
+
+            <label class="program-choice">
+                <input type="radio" name="program" value="3Dmodeling">
+                <span>3D Modelling and Visual Effects</span>
+            </label>
+        </div>
+
+        <div class="sort-option">
+            <div class="sort-program">
+                <label for="class of">Sort by class of</label>
+                <select name="class-of">
+                    <option value="2020" selected="selected">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>  
+                </select>
+            </div>
             
-            <article class="student-basic">
-                <a href="/studentProfile" data-route="/studentProfile"><img src="/img/inna_pro_pic.jpg" alt="professional image" class="student-image"></a>
-                <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Inna Manova</h1></a>
-                <h2 class="student-position">Web Designer</h2>
-            </article>
+            <div class="sort-name">
+                <label for="name">Sort by </label>
+                <select name="by name">
+                    <option>name</option>
+                    <option value="ascending">A to Z</option>
+                    <option value="descending">Z to A</option> 
+                </select>
+            </div>
+        </div>
+        
+        <div class="students-headshot">
+            <section class="all-students">
+                <article class="student-basic">
+                    <a href="/studentProfile" data-route="/studentProfile"><img src="/img/bijaya_pro_pic.png" alt="professional image" class="student-image"></a>
+                    <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Bijaya Gautam</h1></a>
+                    <h2 class="student-position">App System Engineer</h2>
+                </article>
 
-            <article class="student-basic">
-                <a href="/studentProfile" data-route="/studentProfile"><img src="/img/bijay_pro_pic.png" alt="professional image" class="student-image"></a>
-                <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Bijay Karki</h1></a>
-                <h2 class="student-position">Application Engineer</h2>
-            </article>
+                <article class="student-basic">
+                    <a href="/studentProfile" data-route="/studentProfile"><img src="/img/joseph_pro_pic.jpg" alt="professional image" class="student-image"></a>
+                    <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Joseph Lao</h1></a>
+                    <h2 class="student-position">Software Engineer</h2>
+                </article>
+                
+                <article class="student-basic">
+                    <a href="/studentProfile" data-route="/studentProfile"><img src="/img/inna_pro_pic.jpg" alt="professional image" class="student-image"></a>
+                    <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Inna Manova</h1></a>
+                    <h2 class="student-position">Web Designer</h2>
+                </article>
 
-        </section>
+                <article class="student-basic">
+                    <a href="/studentProfile" data-route="/studentProfile"><img src="/img/bijay_pro_pic.png" alt="professional image" class="student-image"></a>
+                    <a href="/studentProfile" data-route="/studentProfile"><h1 class="student-name">Bijay Karki</h1></a>
+                    <h2 class="student-position">Application Engineer</h2>
+                </article>
+            </section>
+        </div>
     </main>
   `;
 };
