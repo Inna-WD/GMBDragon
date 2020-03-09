e991260c39b7004d5b32a0e0855549cd27acda1e
Bijaya Gautam <n01399849@nj-00118-020.humber.org>
2020-03-09 14:32:26 -0400

    Updated style for 404 error.

--- src/components/404.js
+++ src/components/404.js
@@ -3,7 +3,7 @@ import { html } from "../../core/Spaf.js";
 const _404Page = () => {
   console.log("404 page");
   return html`
-    <h1>404 page!!</h1>
+    <h1 class="url-error">Page not found</h1>
   `;
 };
 
