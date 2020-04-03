e70ce6797f3fe57b0d850d75ac2fbd2a5f51a7c6
@Inna <55448200+Inna-WD@users.noreply.github.com>
2020-04-03 16:14:44 -0400

    0304

--- src/components/serviceCitations.js
+++ src/components/serviceCitations.js
@@ -25,12 +25,25 @@ const serviceCitations = () => {
       <article>
             <h2 class="main-text">What are Citations and Backlink services ?</h2>
               <p>
-                    some content
+              The simple definition of a citation is purely the mention or reference of a business’s name, address, and phone number, otherwise acronymed “NAP.” Citations are an important variable in determining the ranking of a location in Google Maps search as well as the local 3-pack shown in Google Search.
+              <br>
+              <br>
+              Google crawls this NAP information wherever it may be found. This includes but is not limited to: local directories, social media sites, chamber of commerce sites, industry directories, press releases, news releases, and other publications. Additionally, Google uses the NAP information mentioned on your business’s website and Google My Business listing as a primary point of reference. For businesses with multiple locations, such as franchises, this underscores a key point in ensuring all of your locations are properly organized on the website.
+              <br>
+              <br>
+              In a nutshell, Google compares the website’s NAP information with that of surrounding sources throughout the web. This mechanism is used to assess the legitimacy of a business’ location. If there are discrepancies in a location’s citation data (i.e. inconsistent phone number or address information), it sends mixed signals to Google and rankings can suffer as a result.
               </p>
 
-            <h2 class="main-text">Title 1</h2>
+            <h2 class="main-text">Backlinks</h2>
                 <p>
-                    Content 1 Content 1Content 1Content 1
+               
+                Similar to citations, backlinks are off-site signals that Google use to assess the credibility and authority of website. Backlinks, also known as external links or just “links,” have defining characteristics, like referring source (or referring domain), anchor text and DoFollow or NoFollow
+                <br>
+                <br>
+                Specifically, referring domains looks at where backlinks are coming from. This includes the particular websites (and how authoritative and relevant it is), as well as where the backlink is located on the site, such as a blog post, advertising graphic, or directory listing. Backlinks from authoritative sites with a high degree of relevancy are gold for SEO.
+                <br>
+                <br>
+                For example, in the case of local restaurant based in Augusta, GA, a backlink coming from an advertisement on Augusta.com’s homepage will probably offer greater SEO value than the backlink from a DexKnows.com listings. In essence, building and earning backlinks is a multi-layered strategy that includes a high level of effort. It’s easy to build listings on relevant directories and social media sites, but it’s often the hard-to-earn links that lend to greater domain authority and overall ranking potential.
                 </p>
 
             <h2 class="main-text">Title 2</h2>
